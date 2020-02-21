4b6969c3469b64ee312f35615232ac0a63ad7629
Aaron Marr <aaron.marr@icloud.com>
2020-02-21 07:29:02 +0000

    wip

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-
-        return view('post', compact('post'));
+        return view('post', [
+            'post' => Post::where('slug', $slug)->firstOrFail()
+        ]);
     }
 }
